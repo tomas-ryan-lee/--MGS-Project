dff257d459f2b80b6f4f301de08635ba7067e5a7
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-09-29 11:59:46 +0200

    KR - Intégration de l'item 'Rations'

---
+++ Assets/Scripts/Items/ItemBlock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBlock : MonoBehaviour
+{
+    // Rotation speed (degrees/sec)
+    [SerializeField]
+    int m_RotateSpeed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, m_RotateSpeed * Time.deltaTime, 0);
+
+    }
+}
